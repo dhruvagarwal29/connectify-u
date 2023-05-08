c93c64d1ad97fd6a3377f645dfb4e9d75c96d870
Dhruv Agarwal <dhruvagar29@gmail.com>
2023-05-07 21:22:55 -0400

    update Feed.js

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,38 +1,35 @@
-import { Avatar } from '@mui/material'
-import React, { forwardRef} from 'react'
-// import InputOption from './InputOption'
-import './Post.css'
-// import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
-// import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
-// import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-// import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
-// import { useSelector } from 'react-redux';
-// import { selectUser } from './features/userSlice';
-const Post = forwardRef(({name, description, message, photoUrl}, ref ) => {
+import { Avatar } from "@mui/material";
+import React, { forwardRef } from "react";
+import InputOption from "./InputOption";
+import "./Post.css";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
+const Post = forwardRef(({ name, description, message, photoUrl }, ref) => {
   return (
-    <div ref = {ref} className='post'>
-        <div className='post__header'>
-            <Avatar src={photoUrl} >{name[0].toUpperCase()}</Avatar>
-            <div className='post__info'>
-                <h2>{name}</h2>
-                <p>{description}</p>
-            </div>
+    <div ref={ref} className="post">
+      <div className="post__header">
+        <Avatar src={photoUrl}>{name[0].toUpperCase()}</Avatar>
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
         </div>
-        
-        <div className='post__body'>
-            <p>{message}</p>
-        </div>
+      </div>
 
-        <div className='post__buttons'>
-            {/* <InputOption Icon = {ThumbUpOutlinedIcon} title="Like" color ="gray"/>
-            <InputOption Icon = {ChatOutlinedIcon} title="Comment" color ="gray"/>
-            <InputOption Icon = {ShareOutlinedIcon} title="Share" color ="gray"/>
-            <InputOption Icon = {SendOutlinedIcon} title="Send" color ="gray"/> */}
-        </div>
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpOutlinedIcon} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
+      </div>
     </div>
+  );
+});
 
-  )
-})
-
-export default Post
+export default Post;
